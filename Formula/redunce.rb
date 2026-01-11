3493ead193507862bda48ba5f858734d1d7af391
Zach Snow <z@zachsnow.com>
2026-01-10 21:49:14 -0600

    Update redunce to v0.1.1

--- Formula/redunce.rb
+++ Formula/redunce.rb
@@ -1,7 +1,7 @@
 class Redunce < Formula
   desc "Find potentially redundant code using vector similarity"
   homepage "https://github.com/ZachSnow/redunce"
-  url "https://github.com/ZachSnow/redunce/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/ZachSnow/redunce/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
